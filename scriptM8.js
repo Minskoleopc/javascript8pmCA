383ec077f6db56fec2e45340386dbdb60527a52b
Minskoleopc <minskoleopc@gmail.com>
2022-12-06 20:31:12 +0530

    added

--- scriptM8.js
+++ scriptM8.js
@@ -1,77 +1,185 @@
 
 
-let x = 10
-console.log(x)
+// let x = 10
+// console.log(x)
 
-// Array
+// // Array
 
-//              0        1      2
-let names = ["chinmay","ram" ,"sham"]
-// console.log(names[0])
-// console.log(names[1])
-// console.log(names[2])
+// //              0        1      2
+// let names = ["chinmay","ram" ,"sham"]
+// // console.log(names[0])
+// // console.log(names[1])
+// // console.log(names[2])
 
-for(let i = 0 ; i < 3 ; i++){
-    //console.log(i) 
-    console.log(names[i])
-    // 1st - 0
-    // 2nd  -1
-    // 3rd  -2 
+// for(let i = 0 ; i < 3 ; i++){
+//     //console.log(i) 
+//     console.log(names[i])
+//     // 1st - 0
+//     // 2nd  -1
+//     // 3rd  -2 
+// }
+
+
+// //            0          1        2      3
+// let city = ["nagpur", "mumbai","pune","nashik"]
+// console.log(city.length)
+
+// console.log(city[0])
+
+// for(let i = 0 ; i < 4 ; i++){
+//     //console.log(i)
+//     console.log(city[i])
+//     // 1st -- 0
+//     // 2nd -- 1
+//     // 3rd -- 2
+//     // 4th -- 3
+// }
+
+// // Program -3 
+// //Object --- type
+
+// // Human 
+// // Property --- age , color , gender , weight 
+// // Method - talk() , walk()
+
+// // Vehicle
+// // Property  - modelNo , color  type ,height
+// // Method   - start()  stop()
+
+// // Bank -- 
+// // Property - accNo , bal , accNAme
+// // Method - withdrawl() , deposit()
+
+// // Javascript object 
+// // Object -- property and method
+// // Method -
+// // Gym ---  Method
+// // Action --- excercise 
+// // return  --- health
+
+
+// // Javascript - Object 
+// // Object property and method
+// // Method- action and return type 
+// // Array ---- Object 
+// // Array - property and method
+// // Array - action and return type
+
+// let cities = ["pune","delhi","nagpur"]
+// let q1 = cities.length
+// console.log(q1)
+
+// let q2 = cities.push("mumbai")
+// console.log(cities)
+// console.log(q2)
+
+//                 0          1          2       3         4
+let vegtables = ["cabbage","tomato","brinjal","potato","cauliflower"]
+console.log(vegtables[1])
+console.log(vegtables[4])
+
+
+for(let i = 0 ; i <  5 ; i++){
+    //console.log(i)
+    console.log(vegtables[i])
+
+    // i - 0
+    // i - 1
+    // i - 2
+    // i - 3
+    // i - 4
 }
 
 
-//            0          1        2      3
-let city = ["nagpur", "mumbai","pune","nashik"]
-console.log(city.length)
-
-console.log(city[0])
+//              0         1         2          3
+let cities = ["delhi","banglore","kolkala" , "pune"]
+console.log(cities[0])
 
 for(let i = 0 ; i < 4 ; i++){
     //console.log(i)
-    console.log(city[i])
-    // 1st -- 0
-    // 2nd -- 1
-    // 3rd -- 2
-    // 4th -- 3
+    console.log(cities[i])
+    // i --- 0
+    // i ----1
+    // i --- 2
+    // i ---- 3
 }
 
-// Program -3 
-//Object --- type
-
+// Types ----
 // Human 
-// Property --- age , color , gender , weight 
-// Method - talk() , walk()
+// Property  - color , age 
+// Method  - walk() , talk()
 
-// Vehicle
-// Property  - modelNo , color  type ,height
-// Method   - start()  stop()
+//Vehicle - 
+//Property - model , color , type 
+//Method   - start() , stop()
 
-// Bank -- 
-// Property - accNo , bal , accNAme
-// Method - withdrawl() , deposit()
-
-// Javascript object 
-// Object -- property and method
-// Method -
-// Gym ---  Method
-// Action --- excercise 
-// return  --- health
+//Bank 
+// Property - accName , accNo
+// Method  - deposit() , withdrawl()
 
 
-// Javascript - Object 
-// Object property and method
-// Method- action and return type 
-// Array ---- Object 
-// Array - property and method
-// Array - action and return tyope
 
-let cities = ["pune","delhi","nagpur"]
-let q1 = cities.length
+// Array -- js 
+// Object 
+// Properties and methods 
+
+
+// Method ---  Gym
+// action -- excercise()
+// return  health()
+
+
+//                 0           1       2       3
+let languages = ["marathi","hindi","sankrit","tamil"]
+console.log(languages.length)
+
+
+//               0       1        2      3 
+let animals = ["tiger","lion","rabbit","snake"]
+
+
+// push()
+// action 
+// return 
+
+let q1 = animals.push("dog")
+console.log(animals)
 console.log(q1)
 
-let q2 = cities.push("mumbai")
-console.log(cities)
+
+// unshift()
+// action()
+// return 
+
+let q2 = animals.unshift("cat")
 console.log(q2)
+console.log(animals)
 
 
 
+// pop()
+// action 
+// return 
+//[ 'cat', 'tiger', 'lion', 'rabbit', 'snake', 'dog' ]
+
+let q3 = animals.pop()
+console.log(q3)
+console.log(animals)
+
+
+let q4 = animals.shift()
+console.log(q4)
+console.log(animals)
+
+
+
+
+
+// shift()
+// action 
+// return type
+
+
+// includes 
+// action 
+// return type 
