d74b87b17897e36523cc2e62a218f90d5da05a76
Minskoleopc <minskoleopc@gmail.com>
2023-01-28 20:23:12 +0530

    added

---
+++ DOM11/script.js
@@ -0,0 +1,48 @@
+//<h3 class="one" name="nm" id="three">Heading Three</h3>
+
+let headThree = document.querySelector('h3')
+let buttonA = document.querySelector('button')
+// headThree.classList.add('two')
+// headThree.classList.remove('one')
+// headThree.classList.toggle('two')
+// headThree.classList.toggle('two')
+// headThree.classList.replace('two','twoA')
+// headThree.classList.forEach(function(className){
+//     console.log(className)
+// })
+
+// let idValue = headThree.getAttribute('id')
+// let byName = headThree.getAttribute('name')
+// console.log(idValue)
+// console.log(byName)
+///////////////////////////////////////////////////////////////////////
+
+
+// headThree.addEventListener('click',function(){
+//     headThree.style.color = "green"
+// })
+
+// mouseover
+// headThree.addEventListener('mouseover',function(){
+//     headThree.style.color = "green"
+// })
+
+// headThree.addEventListener('mouseout',function(){
+//     headThree.style.color = "red"
+// })
+
+// headThree.addEventListener('dblclick',function(){
+//     headThree.style.color = "green"
+// })
+
+
+//  reload the page
+
+buttonA.addEventListener('click',function(){
+    location.reload()
+})
+
+
+
+
+
